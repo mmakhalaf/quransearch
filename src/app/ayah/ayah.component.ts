00f650c32dc0b313d27b6ca521155e623daf010c
Mohamed Massoud <mmakhalaf@gmail.com>
2019-01-09 20:16:11 +0000

    Add empty Ayah component to stop build failure

---
+++ src/app/ayah/ayah.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ayah',
+  templateUrl: './ayah.component.html',
+  styleUrls: ['./ayah.component.css']
+})
+export class AyahComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
