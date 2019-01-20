4203818be8d6413ef9afd9d426979f951970755b
Mohamed Massoud <mmakhalaf@gmail.com>
2019-01-20 10:37:30 +0000

    Remove docs to be in a separate repo

--- prod_build.sh
+++ prod_build.sh
@@ -1,1 +1,3 @@
 ng build --output-path docs --prod --aot --optimization --vendor-chunk --common-chunk --build-optimizer --extract-css --delete-output-path --stats-json
+rm -rvf ../mmakhalaf.github.io/*
+cp -rvf docs/* ../mmakhalaf.github.io
